13295e4a207f63d8b966f097638b20eab4e969ae
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-06-01 00:48:10 -0300

    Criação da rota de listagem dos agendamentos por cliente e por salão - Backend.

--- backend/src/controllers/scheduleController.ts
+++ backend/src/controllers/scheduleController.ts
@@ -26,3 +26,63 @@ export async function createAppointment(req: Request, res: Response ) {
         return res.status(400).json({ error: "Erro ao criar agendamento", details: error.message });
     }
 }
+
+export async function listClientAppointments(req: Request, res: Response) {
+    const clientId = req.users?.uid
+
+    if (!clientId) {
+        return res.status(400).json({ error: "ID do cliente não fornecido" });
+    }
+
+    try {
+        const snapshot = await admin.firestore().collection('appointments')
+            .where('clientId', '==', clientId)
+            .orderBy('date', 'asc')
+            .get();
+
+        const appointments = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+
+        res.status(200).json(appointments);
+    } catch (error: any) {
+        return res.status(400).json({ error: "Erro ao listar agendamentos", details: error.message });
+    }
+}
+
+export async function listAllAppointments(req: Request, res: Response) {
+    const adminUid = req.users?.uid;
+
+    if (!adminUid) {
+        return res.status(401).json({ error: "Usuário não autenticado" });
+    }
+
+    try {
+        // Busca o salonId do admin autenticado
+        const adminDoc = await admin.firestore().collection('users').doc(adminUid).get();
+        const adminData = adminDoc.data();
+
+        if (!adminData || !adminData.salonId) {
+            return res.status(403).json({ error: "Admin não vinculado a nenhum salão" });
+        }
+
+        const salonId = adminData.salonId;
+
+        // Busca os agendamentos do salão do admin autenticado
+        const snapshot = await admin.firestore()
+            .collection('appointments')
+            .where('salonId', '==', salonId)
+            .orderBy('date', 'desc')
+            .get();
+
+        const appointments = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+
+        res.status(200).json({ appointments });
+    } catch (error: any) {
+        return res.status(400).json({ error: "Erro ao listar agendamentos", details: error.message });
+    }
+}
--- backend/src/routes/scheduleRoutes.ts
+++ backend/src/routes/scheduleRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { createAppointment } from "../controllers/scheduleController";
+import { createAppointment, listAllAppointments, listClientAppointments } from "../controllers/scheduleController";
 import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 // Rota para criar agendamento
 router.post("/appointment", authenticateToken, createAppointment);
+
+// Rota para listar agendamentos do cliente
+router.get("/client", authenticateToken, listClientAppointments)
+
+// Rota para listar todos os agendamentos (apenas para administradores)
+router.get("/", authenticateToken, listAllAppointments)
 
 export default router;
