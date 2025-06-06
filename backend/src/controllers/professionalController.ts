13a95c668da8bd7bb4276922dc53a8c2244a735e
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-06-05 21:16:20 -0300

    Criação da rota de definir os horários disponíveis do profissional - Backend.

--- backend/src/controllers/professionalController.ts
+++ backend/src/controllers/professionalController.ts
@@ -18,3 +18,24 @@ export async function updateProfessionalProfile(req: Request, res: Response) {
         return res.status(400).json({ error: "Erro ao atualizar perfil", details: error.message });
     }
 }
+
+export async function updateAvailability(req: Request, res: Response) {
+    const uid = req.users?.uid
+    const { availability } = req.body;
+    
+    if (!uid) {
+        return res.status(401).json({ error: "Usuário não autenticado" });
+    }
+    if (!availability) {
+        return res.status(400).json({ error: "Disponibilidade não fornecida" });
+    }
+
+    try {
+        await admin.firestore().collection('users').doc(uid).update({ availability });
+        await admin.firestore().collection('professionals').doc(uid).update({ availability });
+
+        res.status(200).json({ message: "Disponibilidade atualizada com sucesso" });
+    } catch (error: any) {
+        return res.status(400).json({ error: "Erro ao atualizar disponibilidade", details: error.message });
+    }
+}
--- backend/src/routes/professionalRoutes.ts
+++ backend/src/routes/professionalRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { updateProfessionalProfile } from "../controllers/professionalController";
+import { updateProfessionalProfile, updateAvailability } from "../controllers/professionalController";
 import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 // Rota de atualização de perfil do profissional
 router.put("/profile", authenticateToken, updateProfessionalProfile);
+
+// Rota de atualização de disponibilidade do profissional
+router.put("/availability", authenticateToken, updateAvailability);
 
 export default router;
